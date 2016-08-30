852fc08c33e444c11bc827c9d2c9043b6af94a11
Eleanor Roosevelt <eleanor@gmail.co>
2016-08-30 13:01:46 -0500

    updated finished version with final additions

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -19,3 +19,15 @@ designer_info = {
 	:theme => theme,
 	:compensation => compensation
 }
+
+p designer_info
+
+p "Do you need to change an answer? Please input the value name (example: decor_theme) that you want to edit, or type none"
+input = gets.chomp
+if input == "none"
+else
+	p "input new #{input}"
+	new_input = gets.chomp
+	designer_info[input.to_sym] = new_input
+end
+p designer_info
