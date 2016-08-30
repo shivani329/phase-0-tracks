807eba51a74e34946c8b40c22c5cb4b945f11b9e
Eleanor Roosevelt <eleanor@gmail.co>
2016-08-30 10:32:24 -0500

    finished release5.1

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -13,30 +13,30 @@ p "iNvEsTiGaTiOn".swapcase
 p "zom".gsub('o','oo')
 # => “zoom”
 
-p "enhance".center(14)
+p "enhance".center(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+p "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+p "the usual".insert(9, " suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+p " suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+p "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+p "The mystery of the missing first letter".reverse.chop.reverse
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+p "Elementary,    my   dear        Watson!".squeeze
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+p "z".ord
 # => 122 
-# (What is the significance of the number 122 in relation to the character z?)
+# (What is the significance of the number 122 in relation to the character z? : it is the number that corresponds to the integer ordinal for lowercase z)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+p "How many times does the letter 'a' appear in this string?".count("a")
 # => 4
