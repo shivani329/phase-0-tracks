ea03cfb7a716cf517efafc41d08246693953fd73
Eleanor Roosevelt <eleanor@gmail.co>
2016-09-02 17:47:08 -0500

    Release 2

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -21,32 +21,54 @@ def search(integer,array)
 end
 
 
-int = 765
+int = 100
 arr = [1,2,3,4,5,10,100,765]
-puts search(int,arr))
+puts search(int,arr)
 
-#Release 1
-
-		def fib(n)
+def fib(n)
+	# Start with index of zero, iterate through indices until n value is reached
 	index = 0
 	array = []
 	until index == n
-    if index == 0
-        array << 0
-        index += 1
-    elsif index == 1
-        array << 1
-        index += 1
-    else
-        array << (array[index-2] + array[index-1])
-        index += 1
-    end
+		# if index 0, add 0 to array
+	    if index == 0
+	        array << 0
+	        index += 1
+	    elsif index == 1
+	    	# if index 1, add 1 to array
+	        array << 1
+	        index += 1
+	    else
+	    	# else add values of previous two indices to new value in array
+	        array << (array[index-2] + array[index-1])
+	        index += 1
+	    end
+	end
+	# return completed array
+	array
+end
+
+puts fib(100).last
+
+# Sort array of integers from largest to smallest
+# define method with one input parameter - an array of integers
+# iterate through each index of array 'number'
+# if number is less than value of following index of array, swap
+# repeat until number <= value of following index of array
+# continue iterating through indices
+
+def sorter(array)
+	loop do
+		swapped = false
+		(array.length - 1).times do |i|
+			if array[i] < array[i+1]
+				array[i], array[i+1] = array[i+1], array[i]
+				swapped = true
+			end
+		end
+		break if not swapped
 	end
 	array
 end
 
-fib(100).last
-
-
-
-
+puts sorter([3,500,10,88,34,2,1500])
