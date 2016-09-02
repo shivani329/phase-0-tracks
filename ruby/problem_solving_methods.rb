ae06b2be7705219ae1da0900a2c050a0e84f43d0
Eleanor Roosevelt <eleanor@gmail.co>
2016-09-02 16:15:08 -0500

    Release 0

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -1,24 +1,54 @@
-def search (integer,array)
+#define method - input integer and array 
+#iterate through each index of array
+# using while loop until index 
+# compare value of integer to array at current index 
+# if equal to one another return output of index
+
+
+def search(integer,array)
 	index = 0
-	while index == array.length
-		break if output != nil
+
+	# array.length.times do
+	# finished = nil
+	# output = nil
+
+	# break if (output != nil) || (finished == true)
+
+	while index < array.length
 		output = nil
+		# break if output != nil
 		array.each do |number|
-			if number == integer
+			if array[index] == integer
 				output = index
-				
 			else
-				output = 1
-				index += 1
+				output = nil
+				output
+				# if index == array.length
+					# finished = true
+				# end
 			end
+			# break if number == integer
+
+			# if number != integer
+			# 	output = nil
+			# elsif number == integer
+			# 	output = index
+			# end
 		end
+	if output != nil
+		break
+	end
 	index += 1
 	end
-	
 	output
+end
 
-end				
-
-int = 3
+int = 100
 arr = [1,2,3,4,5,10,100,765]
 puts search(int,arr)
+
+
+
+
+
+
