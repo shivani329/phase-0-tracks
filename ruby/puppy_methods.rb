db942a00e829c6d319a5f4ce10a55e732cc37183
Eleanor Roosevelt <eleanor@gmail.co>
2016-09-06 17:13:24 -0500

    Release 1 and 2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -24,7 +24,10 @@ class Puppy
   	puts "Remmy goes for a #{distance} mile walk"
   end
 
-
+  def initialize 
+  	p "Initializing new puppy instance..."
+  end 
+  #initialize method ran at the beginning of the program without calling it in my driver code
 
 end
 
@@ -38,4 +41,42 @@ Remmy.speak(4)
 Remmy.roll_over
 Remmy.dog_years(35)
 Remmy.walk("three")
+
+
+
+class Basketball
+
+	def initialize 
+		puts "Initializing basketball blabber..."
+	end
+
+	def score(player, number)
+		puts "#{player} shoots for #{number}, and he scores!"
+	end
+
+	def cheer(number)
+		number.times {puts "DE-FENSE!!"}
+	end
+
+end
+
+
+
+
+
+basketball_array = []
+
+50.times do |count|
+	basketball_array << Basketball.new
+end
+
+basketball_array.each do |ball|
+	ball.cheer(5)
+	ball.score("Jimmy Butler", "3")
+end
+
+
+
+
+
 
