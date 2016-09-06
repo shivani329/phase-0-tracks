ec61fce08e1bfc7b525b90a9dff54e1147502f9c
Eleanor Roosevelt <eleanor@gmail.co>
2016-09-06 15:26:01 -0500

    release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,37 @@ class Puppy
     toy
   end
 
+
+  def speak(woof) 
+  	woof.times do
+		puts "Woof!"  		
+  	end
+  end
+
+  def roll_over
+  	puts "Roll over"
+  end
+
+  def dog_years(years)
+  	p years / 7 
+  end
+
+  def walk(distance)
+  	puts "Remmy goes for a #{distance} mile walk"
+  end
+
+
+
 end
+
+
+
+
+Remmy = Puppy.new
+
+Remmy.fetch("ball")
+Remmy.speak(4) 
+Remmy.roll_over
+Remmy.dog_years(35)
+Remmy.walk("three")
 
