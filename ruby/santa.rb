9fc94621e980e5377c92a38e15a93dec29c6ad8b
Eleanor Roosevelt <eleanor@gmail.co>
2016-09-07 13:48:18 -0500

    in progress

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,6 +1,7 @@
 class Santa
 
-	
+	attr_reader :name,:ethnicity,:home,:gender,:age
+	attr_accessor :gender,:ethnicity,:name,:home
 
 	def speak
 		puts "Ho, ho, ho! Haaaappy holidays!"
@@ -50,15 +51,15 @@ end
 	test.eat_milk_and_cookies("choco chip cookie")
 
 
-	test.celebrate_birthday = 20
+	test.celebrate_birthday
 
-	test.get_mad_at = "Donner"
+	test.get_mad_at = "Rudolph"
 	test.gender= "agender"
 	puts "now you identify as #{test.gender}"
 
 
-	def santa_generator()
-	example_name=["mike","peter","cracker-jack","ozwald","johnny","lisle","genie"].shuffle
-	example_genders = ["transgender", "male", "bigender", "female", "gender fulid", "unsure", "N/A"].shuffle
+	# def santa_generator()
+	# example_name=["mike","peter","cracker-jack","ozwald","johnny","lisle","genie"].shuffle
+	# example_genders = ["transgender", "male", "bigender", "female", "gender fulid", "unsure", "N/A"].shuffle
 	
 
