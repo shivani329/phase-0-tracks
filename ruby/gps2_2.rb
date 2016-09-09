b9749a456caaad077674c00bccb75fc7cbf13339
Shivani Kumar <shivani329@gmail.com>
2016-09-09 18:16:30 -0500

    added methods

--- ruby/gps2_2.rb
+++ ruby/gps2_2.rb
@@ -52,29 +52,3 @@ end
 alias add updatedList
 
 
-def listOutput(list)
-	list.each{|item, quantity| p "You need to get #{quantity} #{item}(s)"}
-end
-
-list = mainList("apples eggs carrots")
-list = add("lemonade", list, 2)
-list = add("tomatoes", list, 3)
-list = add("onions", list, 1)
-list = add("ice Cream", list, 4)
-list = remove("lemonade", list)
-list = updatedList("ice Cream", list, 1)
-listOutput(list) 
-
-
-
-
-# Doing this as a solo challenge was tough. I relied on a lot of research to get it done
-# I learned about the alias method and where that can be useful. 
-# I dont really love psuedocoding...I almost prefer to jot it down on a piece of paper, but 
-# I realize that pseduocoding is just a virtual way of jotting things down but in a place
-# that is accessable any time while working on a file. I found myself confused suddnely with hashes
-# vs arrays since time has passed since last week. Ive used them plenty since, but I just use them without
-# putting too much thought in to it...which I probably should start doing more of. I kept wanting to use a class
-# and call instances but that was a part of the challenge. I wasn't sure if i was supposed to make this with a user
-# interface or not, but since it didnt specify I didn't do so. I need to really go back and go through all of the ruby material
-# we have covered, as I am getting nervous for the week 7 assessment. But, I'm working on it! 
