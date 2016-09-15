2046240e373b9f34af126c0289c952573ca6b04c
Shivani Kumar <shivani329@gmail.com>
2016-09-15 13:34:19 -0500

    finished release 1 and 2, started 3

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+var colors = ["blue", "yellow", "aqua purple", "red"];
+
+var name = ["Jon", "ed", "nancy", "black beauty"]
+
+name.push("secretariat");
+
+colors.push("egg shell white");
+
+console.log(colors,name);
