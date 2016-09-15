b6b74fdc617f88d05c5eaa4d41bd5e8b27cc7be7
Shivani Kumar <shivani329@gmail.com>
2016-09-15 13:47:38 -0500

    finished release 2, adding extra features

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,9 +1,41 @@
 var colors = ["blue", "yellow", "aqua purple", "red"];
 
-var name = ["Jon", "ed", "nancy", "black beauty"]
+var names = ["Jon", "ed", "nancy", "black beauty"]
 
-name.push("secretariat");
+names.push("secretariat");
 
 colors.push("egg shell white");
 
-console.log(colors,name);
+console.log(colors,names);
+
+
+var stable = {};
+
+for (var i=0; i<names.length; i++)
+{
+	stable [names[i]] =colors [i];
+
+}
+
+console.log(stable);
+
+function Car (make, model, year, color, isUsed) {
+	this.make=make;
+	this.model=model;
+	this.year=year;
+	this.color=color;
+	this.isUsed=isUsed;
+	this.describe= function() 
+	{ 
+		console.log("I am a " + this.isUsed + " " + this.color + " " + this.year + " " + this.make + " " + this.model) 
+		console.log("Drive safe!")};
+	};
+
+var Ruby = new Car ("Lexus", "RX350", "2009", "red", "previously owned");
+Ruby.describe()
+
+var Barney = new Car ("Chrysler", "PT Cruiser", "2001", "purple", "previously owned");
+Barney.describe()
+
+
+
