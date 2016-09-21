40bef256dcd36741a4d70a9b99403759e4a7056a
Shivani Kumar <shivani329@gmail.com>
2016-09-21 14:25:44 -0500

    release 5.1

---
+++ ruby/method_detective-1.rb
@@ -0,0 +1,43 @@
+# Replace in the "<???>" with the appropriate method (and arguments, if any).
+# Uncomment the calls to catch these methods red-handed.
+
+# When there's more than one suspect who could have
+# committed the crime, add additional calls to prove it.
+
+#chipmunks 
+
+
+p "iNvEsTiGaTiOn".swapcase
+# => “InVeStIgAtIoN”
+
+p "zom".(1, "o")
+# => “zoom”
+
+p "enhance".center(15)
+# => "    enhance    "
+
+p "Stop! You’re under arrest!".upcase
+# => "STOP! YOU’RE UNDER ARREST!"
+
+p "the usual".insert(9, "suspects")
+#=> "the usual suspects"
+
+p " suspects".prepend("the usual")
+# => "the usual suspects"
+
+p "The case of the disappearing last letter".chop
+# => "The case of the disappearing last lette"
+
+p "The mystery of the missing first letter".reverse.chop.reverse
+# => "he mystery of the missing first letter"
+
+p "Elementary,    my   dear        Watson!".squeeze(" ")
+# => "Elementary, my dear Watson!"
+
+p "z".ord
+# => 122 
+# (What is the significance of the number 122 in relation to the character z?)
+# it is the number it correlates with in ASCII code, lowercase letters are assigned by byte numbers 97-122
+
+p "How many times does the letter 'a' appear in this string?".count("a")
+# => 4
