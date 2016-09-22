1e17b0ff4250fe1621696f6ea0b24624d3a3fc59
Shivani Kumar <shivani329@gmail.com>
2016-09-22 09:21:57 -0500

    create nested data structure

---
+++ ruby/nested_data_structures-1.rb
@@ -0,0 +1,34 @@
+highway = {
+
+  
+
+ lane_1: {
+       car_model: "Lexus",
+       car_color: "Red",
+       type: {
+           year: 2009,
+           number_doors:[2, 4],
+           brake_lights: ["Brake lights are on", "Brake lights are off"]
+        },
+    },
+
+    lane_2: {
+       car_model: "Civic",
+       car_color: "White",
+       type: {
+           year: 2016,
+           number_doors:[2, 4],
+           brake_lights: ["Brake lights are on", "Brake lights are off"]
+        },
+    },
+
+    lane_3: {
+       car_model: "Audi",
+       car_color: "Blue",
+       type: {
+           year: 2011,
+           number_doors:[2, 4],
+           brake_lights: ["Brake lights are on", "Brake lights are off"]
+        },
+     },
+}
