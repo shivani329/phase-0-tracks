671909c8e3d7ec800b3fbbed42f10ca4b02f30e5
Shivani Kumar <shivani329@gmail.com>
2016-09-22 17:26:38 -0500

    add bubble sort/release2/finish5.6

--- ruby/problem_solving_methods-1.rb
+++ ruby/problem_solving_methods-1.rb
@@ -1,3 +1,5 @@
+#release 0
+
 #define method - input integer and array 
 #iterate through each index of array
 
@@ -29,7 +31,7 @@ int = 5
 arr = [1,2,3,4,5,10,100,765]
 puts search(int,arr)
 
-
+#release 1
 def fibonacci(n)
 	#index starts at 0, iterate through until the value of n is reached 
 	index = 2
@@ -42,3 +44,30 @@ def fibonacci(n)
 	array
 end
 puts fibonacci(100).last
+
+
+#release 2
+
+#sort the array from largest to smallest 
+#define method with one parameter (an array of integers)
+#iterate through each (loop do) index number of array
+#if number is less than the value of following index, swap
+# repeat until number is less than or equal to value of the following index 
+
+def bubble_sort(array)
+	loop do 
+		swapped = false
+		number = array.length-1
+		number.times do |i|
+			if array[i] < array[i+1]
+					array[i], array[i+1] = array[i+1], array[i]
+				swapped = true
+			end
+		end
+		break if not swapped
+	end
+	array
+end
+
+puts bubble_sort([45,17,9,12,30])
+
