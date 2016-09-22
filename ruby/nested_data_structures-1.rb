0219d7cf14fedc59b6d77470343e5ba6212691f2
Shivani Kumar <shivani329@gmail.com>
2016-09-22 09:22:18 -0500

    add driver code/finish release5.4

--- ruby/nested_data_structures-1.rb
+++ ruby/nested_data_structures-1.rb
@@ -32,3 +32,13 @@ highway = {
         },
      },
 }
+puts "What kind of car is in lane 1?"
+p highway [:lane_1] [:car_model]
+puts "How many doors does the car in lane 1 have?"
+p highway [:lane_1][:type][:number_doors][1]
+puts "What color is the car in lane 2?"
+p highway [:lane_2] [:car_color]
+puts "What year is the car in lane 3?"
+p highway [:lane_3] [:type] [:year]
+puts "Are the brake lights on the car in lane 2 on?"
+p highway [:lane_2] [:type] [:brake_lights] [0]
