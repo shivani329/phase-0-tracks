89e72781b47ac826dd54f37015f2c09bc3586e33
Shivani Kumar <shivani329@gmail.com>
2016-10-02 13:55:24 -0400

    create a module and two classes/release3

--- ruby/shout-1.rb
+++ ruby/shout-1.rb
@@ -1,17 +1,41 @@
+# module Shout
+# 	def self.yell_angrily(words)
+# 		words + "!!!" + " :("
+# 	end
+
+# 	def self.yelling_happily(words)
+# 		words + "!!!" + " :D"
+# 	end
+
+# end
+
+
+# puts Shout.yell_angrily("I'm angry")
+# puts Shout.yelling_happily("I'm so happy")
+
+# Shout Module Mixin
+
 module Shout
-	def self.yell_angrily(words)
-		words + "!!!" + " :("
+	def good_job(compliment)
+		"You passed, " + compliment + "!!" + " :)"
 	end
 
-	def self.yelling_happily(words)
-		words + "!!!" + " :D"
+	def next_time(suggestion)
+		"You did not pass, " + suggestion + "!!" + " :D"
 	end
-
 end
 
+class TeacherPass
+	include Shout
+end
 
-puts Shout.yell_angrily("I'm angry")
-puts Shout.yelling_happily("I'm so happy")
+class TeacherFail
+	include Shout
+end
 
-Shout Module Mixin
+#Driver code
 
+puts TeacherPass.new.good_job("A+ ...you did great")
+puts TeacherFail.new.next_time("try studying chapter 2 a bit more")
+puts TeacherPass.new.good_job("Looks like you understand the material")
+puts TeacherFail.new.next_time("work on your vocabulary choice")
