235b923a58ce50f4162ed0b952c8dc6609203649
Shivani Kumar <shivani329@gmail.com>
2016-10-16 15:53:56 -0500

    created user interface

--- databases/todo_list/Todo_list.rb
+++ databases/todo_list/Todo_list.rb
@@ -83,3 +83,65 @@ end
 db.execute(create_table_cmd)
 
 
+puts "\n To Do List: \n
+
+1: Add a task
+2: Remove a task
+3: Mark as completed
+4: Show entire list 
+5: Show finished tasks
+6: Show unfinished tasks
+7: Clear list"
+
+valid_input = false
+until valid_input
+	puts "\nInput a number between 1 to 7, followed by the enter key to begin your list or type 'exit'"
+	input = gets.chomp
+	if input == "exit"
+		valid_input = true
+	elsif input == "1"
+		puts "To do list: "
+		to_do = gets.chomp
+		puts "Notes: "
+		notes = gets.chomp
+		add_list(db, table_cmd, to_do, notes)
+		puts "\nFull list: "
+		print_list(db)
+	elsif input == "2"
+		puts "\nWhich item do you want to delete?"
+		puts "\n Current list:"
+		print_list(db)
+		puts "\nEnter the number, followed by 'Enter' to delete the task from the list: "
+		id = gets.chomp.to_i
+		delete(db, delete_cmd, id)
+		puts "\nNew list:"
+		print_list(db)
+	elsif input == "3"
+		puts "\nWhich item did you complete?"
+		puts "\n Current List:"
+		print_list(db)
+		puts "\nEnter the number, followed by 'Enter' to update the task to 'finished': "
+		id = gets.chomp.to_i
+		update(db, update_cmd, id)
+		puts "\n New list:"
+		print_list(db)
+	elsif input == "4"
+		puts "\n Full list:"
+		print_list(db)
+	elsif input == "5"
+		puts "\nUnfinished tasks:"
+		p_unfinished(db)
+	elsif input == "6"
+		puts "\nFinished tasks:"
+		p_finished(db)
+	elsif input == "7"
+		puts "\nCleared list"
+		puts "\n Full list:"
+		delete_finished(db, delete_finished_cmd)
+		print_list(db)	
+	else
+		puts "\nUSER'S INPUT INVALID."
+	end
+end
+
+
