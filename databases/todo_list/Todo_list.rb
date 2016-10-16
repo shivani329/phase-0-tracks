a8c385ee80639e95f77e209e333626a5abd08df2
Shivani Kumar <shivani329@gmail.com>
2016-10-16 15:53:33 -0500

    added methods

--- databases/todo_list/Todo_list.rb
+++ databases/todo_list/Todo_list.rb
@@ -35,3 +35,51 @@ drop_cmd = <<-SQL
 	DROP TABLE todolist
 SQL
 
+def add_list(db, table_cmd, to_do, notes)
+	db.execute(table_cmd, [to_do, notes])
+end
+
+def delete(db, delete_cmd, id)
+	db.execute(delete_cmd, id)
+end
+
+def update(db, update_cmd, id)
+	db.execute(update_cmd, id)
+end
+
+def delete_finished(db, delete_finished_cmd)
+	db.execute(delete_finished_cmd)
+end
+
+def print_list(db)
+	list = db.execute("SELECT * FROM todolist")
+	list.each do |todo|
+		puts todo["id"].to_s + ": " + todo["to_do"]
+		puts "Detail: " + todo["notes"]
+		if todo["quit"] == 1
+			puts "Status: Completed"
+		elsif todo["quit"] == 0
+			puts "Status: Incomplete"
+		end	
+	end
+end
+
+def p_unfinished(db)
+	list = db.execute("SELECT * FROM todolist WHERE NOT quit")
+	list.each do |todo|
+		puts todo["id"].to_s + ": " + todo["to_do"]
+		puts "Detail: " + todo["notes"]
+	end
+end
+
+def p_finished(db)
+	list = db.execute("SELECT * FROM todolist WHERE quit")
+	list.each do |todo|
+		puts todo["id"].to_s + ": " + todo["to_do"]
+		puts "Detail: " + todo["notes"]
+	end
+end
+
+db.execute(create_table_cmd)
+
+
